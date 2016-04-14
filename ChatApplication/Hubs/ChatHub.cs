e3d23ae4d322f80847f30699bfade8cca7a76ab5
Shaun Grech <shaun.grech13@gmail.com>
2016-04-14 10:26:19 +0200

    First Commit

---
+++ ChatApplication/Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ChatApplication.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string name, string message, string roomName)
+        {
+            Clients.Group(roomName).GetMessage(name, message);
+        }
+
+        public Task JoinRoom(string roomName)
+        {
+            return Groups.Add(Context.ConnectionId, roomName);
+        }
+
+        public Task LeaveRooom(string roomName)
+        {
+            return Groups.Remove(Context.ConnectionId, roomName);
+        }
+    }
+}
